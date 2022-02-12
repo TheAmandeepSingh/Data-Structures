c39cf2ee1a1b5079fb98dcda7d710e3e823684cf
TheAmandeepSingh <amandeep.gini@yahoo.com>
2022-02-12 20:12:29 +0530

    LongestWordInADictionary

---
+++ Arrays/Problems/LongestWordinDictionary.cpp
@@ -0,0 +1,15 @@
+/**
+ * Given a string "atdoer" and a dictionary {"to", "toe", "top"}, in this case answer will be "toe", 
+ * what is the maximum length word that can be formed using the letters in the string,
+ * and that word must be present in dictionary as well.
+ * 
+ *                                          OR
+ * 
+ * Given a string s and a string array dictionary, return the longest string in the dictionary 
+ * that can be formed by deleting some of the given string characters. 
+ * If there is more than one possible result, return the longest word with the smallest 
+ * lexicographical order. If there is no possible result, return the empty string.
+ * 
+ * Input: s = "abpcplea", dictionary = ["ale","apple","monkey","plea"]
+ * Output: "apple"
+ **/
